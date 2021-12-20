f5d5485c330895512076819b490a1e575edc91ec
Hannah McCullough <hmac091@gmail.com>
2021-12-20 16:49:59 -0700

    commit tree 2e7483ce5ce4f1be37ec7f409707497a29ad0478

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then((data) => setPlants(data))
+  }, [])
+
+  function onNewPlantSubmit(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+  
+  const plantsToDisplay = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(search)
+  });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm
+        onNewPlantSubmit={onNewPlantSubmit}
+      />
+      <Search 
+        search={search}
+        onPlantSearch={setSearch} 
+      />
+      <PlantList 
+        plants={plantsToDisplay}
+      />
     </main>
   );
 }
